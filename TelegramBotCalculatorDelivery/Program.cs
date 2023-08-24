1e5129dd5ee830e81cfaf5b2b710feac4d4f5d2b
Urryy <yura.yutsko@mail.ru>
2023-08-24 19:47:17 +0300

    ref

--- TelegramBotCalculatorDelivery/Program.cs
+++ TelegramBotCalculatorDelivery/Program.cs
@@ -1,18 +1,23 @@
+using System.Diagnostics;
 using TelegramBotCalculatorDelivery;
 using TelegramBotCalculatorDelivery.Service;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.WebHost.ConfigureKestrel(opt => opt.ListenLocalhost(7000));
+Console.WriteLine("asdasdasd 1");
+Debug.WriteLine("asdasdasd 1");
 // Add services to the container.
 builder.Services.AddSingleton<TelegramBot>();
 builder.Services.AddScoped<ITelegramBotService, TelegramBotService>();
 builder.Services.AddControllers();
 var app = builder.Build();
-
+Debug.WriteLine("asdasdasd 2");
+Console.WriteLine("asdasdasd 2");
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
+Debug.WriteLine("GET service 2");
+Console.WriteLine("GET service 2");
 app.Services.GetRequiredService<TelegramBot>().GetBot().Wait();
 
 app.UseRouting();
